c12ed5bc47e9b98fae6b1ce3cf52d1af8cbf082a
nickolos <silakov.nikolay@gmail.com>
2021-08-22 21:44:31 +0300

    init

---
+++ src/main/java/com/example/prst/PrStApplication.java
@@ -0,0 +1,13 @@
+package com.example.prst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrStApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrStApplication.class, args);
+    }
+
+}
